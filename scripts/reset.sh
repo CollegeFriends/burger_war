83cb3ea3a99340efda84f879ac38d1f096aa0fa2
Yoda M <m_yodai_apple@hotmail.com>
2020-08-08 22:46:49 +0900

    テスト走行の実装 （とりあえずON/OFF制御で目的地に直線的に進む）

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,8 +1,19 @@
-rosnode kill send_id_to_judge
-rosnode kill /enemy_bot/send_id_to_judge
-rosnode kill /enemy_bot/turtlebot3_teleop_keyboard
+# rosnode kill send_id_to_judge
+# rosnode kill /enemy_bot/send_id_to_judge
+# rosnode kill /enemy_bot/turtlebot3_teleop_keyboard
+
+
+
+# ROS関係のセットアップ
+source /opt/ros/kinetic/setup.bash
+source ~/catkin_ws/devel/setup.bash
+export GAZEBO_MODEL_PATH=$HOME/catkin_ws/src/burger_war/burger_war/models/
+export TURTLEBOT3_MODEL=burger
+
+
 rosnode cleanup
 rosservice call /gazebo/reset_simulation "{}"
-bash judge/test_scripts/reset_server.sh localhost:5000 you enemy
-bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy
-gnome-terminal -e "roslaunch burger_war restart_sim.launch"
+# bash judge/test_scripts/reset_server.sh localhost:5000 you enemy
+# bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy
+# roslaunch burger_war setup_test_run.launch
+# gnome-terminal -e "roslaunch burger_war restart_sim.launch"
