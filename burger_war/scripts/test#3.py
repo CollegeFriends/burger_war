fa77434c976799ad703411238f6bbe678803564e
Yoda M <m_yodai_apple@hotmail.com>
2020-08-16 11:45:18 +0900

    バグ修正

--- burger_war/scripts/test#3.py
+++ burger_war/scripts/test#3.py
@@ -27,21 +27,21 @@ try:
         a.put_goal(-0.75,-0.75,None)            
         a.put_goal(-1.3,0.0,None)                
         a.put_goal(-0.53,-0.3,-math.pi/2.0)    # 3
-        # a.put_goal(-0.5,0,0.0)                 # 4
+        a.put_goal(-0.5,0,0.0)                 # 4
         a.put_goal(-0.53,0.3,math.pi/2.0)      # 5
         a.put_goal(-1.3,0.0,None)
         a.put_goal(-0.75,0.75,None)            
         a.put_goal(-0.53, 0.83,-math.pi/2.0)    # 6        
         
-        # a.put_goal(0.0,1.3,None)
-        # a.put_goal(0,0.5,-math.pi/2.0)         # 7
-        # a.put_goal(0.0,1.3,None)                
+        a.put_goal(0.0,1.3,None)
+        a.put_goal(0,0.5,-math.pi/2.0)         # 7
+        a.put_goal(0.0,1.3,None)                
 
         a.put_goal(0.53,0.83,-math.pi/2.0)      # 8
         a.put_goal(0.75,0.75,None)            
         a.put_goal(1.3,0.0,None)                
         a.put_goal(0.5,0.3,math.pi/2.0,timeout=30.0)       # 9
-        # a.put_goal(0.5,0,math.pi)                           # 10
+        a.put_goal(0.5,0,math.pi)                           # 10
         a.put_goal(0.5,-0.3,-math.pi/2.0,timeout=35.0)     # 11
         a.put_goal(1.3,0.0,None)
         a.put_goal(0.75,-0.75,None)            
--- scripts/setup_test.sh
+++ scripts/setup_test.sh
@@ -9,6 +9,7 @@ Usage: $(basename "$0") [OPTION]...
   -h        Display help
   -e        sim with empty field
   -j        sim with judge  
+  -v        sim with versus mode
   Default   sim with marker field (but without enemy)
 EOM
   exit 2
@@ -22,10 +23,10 @@ EOM
 
 flg_e=""
 flg_j=""
-
+flg_v=""
 # 通常は、審判なしモードで起動
 
-while getopts ejh OPT; do
+while getopts ejvh OPT; do
     case "$OPT" in
         e)       
             # -e で何もない空間で起動
@@ -33,9 +34,15 @@ while getopts ejh OPT; do
             ;;
         j)
             # -j で審判サーバー有りで起動
-            flg_j="JUDGE"
+            flg_j="TRUE"
             ;;
-         h|*)
+        v)
+            # -v で対戦
+            echo HEY
+            flg_v="TRUE"
+            ;;
+        h|*)
+            echo $OPT
             usage
             ;;
     esac
@@ -43,19 +50,25 @@ done
 
 echo $flg_e
 echo $flg_j
+echo $flg_v
 # robotの対戦環境の作成
 if [ $flg_e = "TRUE" ]; then
     # eオプションの時は、マーカーなしで起動 / jオプションは無視
     roslaunch burger_war setup_test_run_empty.launch
 else
-    if [ $flg_j = "JUDGE" ]; then
+    if [ $flg_j = "TRUE" ]; then
         # 通常は、マーカー有り・敵有りで起動    
         gnome-terminal -e "python judge/judgeServer.py"
         gnome-terminal -e "python judge/visualizeWindow.py"
         bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy        
         roslaunch burger_war setup_test_run_enemy.launch
     else
-        # 通常は、マーカー有り・敵なしで起動    
-        roslaunch burger_war setup_test_run.launch
+        if [ $flg_v = "TRUE" ]; then
+            # 対戦モード
+            roslaunch burger_war setup_test_run_enemy.launch
+        else
+            # 自機だけのテストランモード
+            roslaunch burger_war setup_test_run.launch
+        fi        
     fi
 fi
