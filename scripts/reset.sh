42ce73f939529fbe6edb14a45d1458b7af5e52d0
Yoda M <m_yodai_apple@hotmail.com>
2020-09-04 21:52:22 +0900

    リセット・リスタートのバッチファイルを作成

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,7 +3,7 @@ rosnode kill /enemy_bot/send_id_to_judge
 rosnode kill /enemy_bot/turtlebot3_teleop_keyboard
 rosnode cleanup
 rosservice call /gazebo/reset_simulation "{}"
-chmod +x burger_war/scripts/*.py
+chmod +x burger_controller/scripts/*.py
 
 
 #!/bin/bash
@@ -33,4 +33,12 @@ case $VALUE_SIDE in
     "r" ) bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy ;;
     "b" ) bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 enemy you ;;
     * ) bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy ;;
+esac
+
+
+# robot
+case $VALUE_SIDE in
+    "r" ) gnome-terminal -e "roslaunch burger_war restart_sim.launch" ;;
+    "b" ) gnome-terminal -e "roslaunch burger_war restart_sim_blue.launch" ;;
+    * ) gnome-terminal -e "roslaunch burger_war restart_sim.launch" ;;
 esac
